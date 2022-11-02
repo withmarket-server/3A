c8a352da3b24fe4dba8686437fe66de28f12d6f7
성지혜 <jihyeseong@seongjihyeui-MacBookAir.local>
2022-11-02 10:14:24 +0900

    init commit

---
+++ src/main/java/com/market/aaa/AaaApplication.java
@@ -0,0 +1,13 @@
+package com.market.aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaaApplication.class, args);
+	}
+
+}
